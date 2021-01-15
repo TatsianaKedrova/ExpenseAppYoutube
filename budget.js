13c217cd05df211f4d10cbf93e89dbd8f01b00d3
Tatiana Kedrova <Baletrot@gmail.com>
2021-01-15 18:51:05 +0200

    Changes in js

--- budget.js
+++ budget.js
@@ -2,3 +2,10 @@
 const balanceEl = document.querySelector(".balance .value");
 const incomeTotalEl = document.querySelector(".income-total");
 const outcomeTotalEl = document.querySelector(".outcome-total");
+const incomeEl = document.querySelector("#income");
+const expenseEl = document.querySelector("#expense");
+const allEl = document.querySelector("#all");
+const incomeList = document.querySelector("#income .list");
+const expenseList = document.querySelector("#outcome .list");
+const allList = document.querySelector("#all .list");
+const chartEl = document.querySelector(".chart");
